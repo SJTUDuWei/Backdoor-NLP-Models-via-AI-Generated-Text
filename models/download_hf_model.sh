ec52f50e518338f10991e0f0da2afc5e04677816
DW <51240996+SJTUDuWei@users.noreply.github.com>
2025-07-04 16:56:53 +0800

    Add files via upload

---
+++ models/download_hf_model.sh
@@ -0,0 +1,5 @@
+huggingface-cli download --resume-download openai-community/gpt2 --local-dir ./gpt2 --local-dir-use-symlinks False
+huggingface-cli download --resume-download google-bert/bert-base-uncased --local-dir ./bert-base-uncased --local-dir-use-symlinks False
+huggingface-cli download --resume-download openai-community/gpt2-large --local-dir ./gpt2-large --local-dir-use-symlinks False
+huggingface-cli download --resume-download sentence-transformers/all-mpnet-base-v2 --local-dir ./sentence-transformers_all-mpnet-base-v2 --local-dir-use-symlinks False
+
